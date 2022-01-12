e3887a52f7358ec4cf053a8eb588568e44f47e39
David Gomez <davidgomezfrieiro@gmail.com>
2022-01-12 13:42:26 +0100

    recuperar data y calculo

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function (e) {
-
+  
   // Para que sirve 'preventDefault'? Intenta averiguarlo
   e.preventDefault();
-
-  console.log("Valor del input: ", e.target.value)
-
+  
+  console.log("Valor del input: ", e.target.weight.value + "Kg", e.target.height.value + "cm")
+  
   // Tu código aquí --->
-
-
+  
+  const height = form.elements['height'];
+  const weight = form.elements['weight'];
+  let weightValue = weight.value;
+  let heightValue = height.value;
+  const imc = weightValue / ((heightValue * heightValue) / 10000);
+  
+  let results = document.querySelector("#results")
+  results.innerHTML = imc;
+  
+  
 });
